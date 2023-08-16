99816c4a95a406ef4dd5fed71cfa03eeae79e6bc
leowitcroz <leowitcroz@gmail.com>
2023-08-16 18:44:08 -0300

    match statement

--- toDo.py
+++ toDo.py
@@ -1,11 +1,24 @@
-prompt = 'Enter a todo: '
+prompt = 'Type add or show or exit: '
 todos = []
 
 while True:
-	todo = input(prompt)
-	todos.append(todo.title())
-	print(todos)
+	user_action = input(prompt)
+	user_action = user_action.strip()
+	match user_action:
+		case 'add' :
+			todo = input("Enter a toDo: ")
+			todos.append(todo.title())
+		case 'show':
+			for item in todos:
+				print(item)
+		case 'exit':
+			print("bye")
+			break
+
+	
+		
 
 
 
 
+ 
