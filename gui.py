87c1a8a19d8369eda3429ed658859198d7f58852
leowitcroz <leowitcroz@gmail.com>
2023-09-15 15:23:01 -0300

    gui add functions, close window

--- gui.py
+++ gui.py
@@ -1,10 +1,24 @@
-# import functions
+import modules.functions as functions
 import PySimpleGUI
 
 label = PySimpleGUI.Text('Type in a to-do')
-input_box = PySimpleGUI.InputText(tooltip='Enter todo')
+input_box = PySimpleGUI.InputText(tooltip='Enter todo', key='todo')
 add_button = PySimpleGUI.Button('Add')
 
-window = PySimpleGUI.Window('My To-Do App', layout=[[[label], [input_box , add_button]]])
-window.read()
-window.close()
+window = PySimpleGUI.Window('My To-Do App', 
+                            layout=[[label], [input_box , add_button]], 
+                            font=('Helvitica', 12))
+
+while True:
+    event, values = window.read()
+    match event:
+        case 'Add':
+            todos = functions.getTodo()
+            new_todo = values['todo'] + '\n'
+            todos.append(str(new_todo).title())
+            functions.writeTodo(todos)
+            print(todos)
+        case PySimpleGUI.WIN_CLOSED:
+            break
+        
+window.close()  
--- modules/functions.py
+++ modules/functions.py
@@ -1,38 +1,46 @@
 def showAllTodos():
-	number = 0
-	file = open('todos.txt', 'r')
-	todos = file.readlines()
-	for item in todos:
-		number += 1
-		print(f'- {number}: {item}')
-		number = number
-		print('-' * 40 )
-	return todos
+    number = 0
+    file = open('todos.txt', 'r')
+    todos = file.readlines()
+    for item in todos:
+        number += 1
+        print(f'- {number}: {item}')
+        number = number
+        print('-' * 40 )
+    return todos
 
 def writeTodo(todos):
-	file = open('todos.txt', 'w') 
-	file.writelines(todos)
+    file = open('todos.txt', 'w') 
+    file.writelines(todos)
 
 def addTodo():
-	todo = input("Enter a toDo: ") + '\n'
-	file = open('todos.txt', 'r')
-	todos = file.readlines()
-	todos.append(todo.title())
-	writeTodo(todos)
+    todo = input("Enter a toDo: ") + '\n'
+    file = open('todos.txt', 'r')
+    todos = file.readlines()
+    todos.append(todo.title())
+    writeTodo(todos)
 
 def editTodo():
-	file = open('todos.txt', 'r')
-	todos = file.readlines()
-	
-	print('Here are your current ToDos:' + '\n')
+    file = open('todos.txt', 'r')
+    todos = file.readlines()
+    
+    print('Here are your current ToDos:' + '\n')
 
-	showAllTodos()
+    showAllTodos()
 
-	number = int(input('Number of the to Do to edit: '))
-	number  = number - 1
+    number = int(input('Number of the to Do to edit: '))
+    number  = number - 1
 
-	newTodo = input(f'Former to Do is {todos[number].rstrip()}, write the edition for this to Do: ') + '\n'
-	todos[number] = newTodo.title()
+    newTodo = input(f'Former to Do is {todos[number].rstrip()}, write the edition for this to Do: ') + '\n'
+    todos[number] = newTodo.title()
 
-	writeTodo(todos)
-	print('-' * 40 )
+    writeTodo(todos)
+    print('-' * 40 )
+
+def getTodo():
+    file = open('todos.txt', 'r')
+    todos = file.readlines()
+    return todos
+    
+    
+    
