291eb4a148b3f88c15f8ed8cbe25f1b47d256045
leowitcroz <leowitcroz@gmail.com>
2023-09-18 13:14:15 -0300

    finish module 1

--- web.py
+++ web.py
@@ -7,8 +7,6 @@ todos = functions.getTodo()
 def add_todo():
     todo = st.session_state["new_todo"] + '\n'
     todos.append(str(todo).title()) 
-    
-    
     functions.writeTodo(todos)
 
 
@@ -16,7 +14,13 @@ st.title('My Todo App')
 st.subheader('This is my todo app.')
 st.write('This app will increase you productivity')
 
-for todo in todos:
-    st.checkbox(todo)
+for index ,todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.writeTodo(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+        
     
 st.text_input(label='',placeholder='Add Todo...', on_change=add_todo, key='new_todo')
