3e52a5f2126218dcf26e73b6b777c2d4a4decfc9
leowitcroz <leowitcroz@gmail.com>
2023-10-27 16:09:18 -0300

    storing data with SQL

--- webscrapping/main.py
+++ webscrapping/main.py
@@ -1,10 +1,14 @@
+import time
 import requests
 import selectorlib
+import sqlite3
 
 
 URL = 'https://programmer100.pythonanywhere.com/tours/'
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+connection = sqlite3.connect('webscrapping/data.db')
 
 def scrape(url):
     ##Scrape de page soucer from the URL
@@ -21,19 +25,30 @@ def send_email():
     print('Email Sent')
     
 def store(extracted):
-    with open('webscrapping/data.txt', 'a') as file:
-        file.write(extracted + '\n')
+    row = extracted.split(',')
+    row = [item.strip() for item in row]
+    cursos = connection.cursor()
+    cursos.execute("INSERT INTO events VALUES(?,?,?)",row)
+    connection.commit()
         
 def read(extracted):
-    with open('webscrapping/data.txt','r') as file:
-        return file.read()
+    row = extracted.split(',')
+    row = [item.strip() for item in row]
+    band,city,date = row
+    cursos = connection.cursor()
+    cursos.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band,city,date))
+    rows = cursos.fetchall()
+    print(rows)
+    return rows
 
 if __name__ == '__main__':
-    scraped = scrape(URL)
-    extracted = extract(scraped)
-    print('extracted: ', extracted)
-    content = read(extracted)
-    if extracted != 'No upcoming tours':
-        if  extracted not in content:
-            store(extracted)
-            send_email()
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print('extracted: ', extracted)
+        if extracted != 'No upcoming tours':
+            row = read(extracted)          
+            if not row:
+                store(extracted)
+                send_email()
+        time.sleep(2)
