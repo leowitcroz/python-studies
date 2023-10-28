8c1faf40c5e99c567dc948189ddb4a8f0c1e7973
leowitcroz <leowitcroz@gmail.com>
2023-10-28 14:49:59 -0300

    starting with OOP

--- webscrapping/main.py
+++ webscrapping/main.py
@@ -10,45 +10,53 @@ HEADERS = {
 
 connection = sqlite3.connect('webscrapping/data.db')
 
-def scrape(url):
-    ##Scrape de page soucer from the URL
-    response = requests.get(url,HEADERS)
-    source = response.text
-    return source
 
-def extract(source):
-   extractor = selectorlib.Extractor.from_yaml_file('webscrapping\extract.yaml')
-   value = extractor.extract(source)['tours']
-   return value
+
+class Event:
+    def scrape(self,url):
+        ##Scrape de page soucer from the URL
+        response = requests.get(url,HEADERS)
+        source = response.text
+        return source
+
+    def extract(self,source):
+        extractor = selectorlib.Extractor.from_yaml_file('webscrapping\extract.yaml')
+        value = extractor.extract(source)['tours']
+        return value
+    
 
 def send_email():
     print('Email Sent')
     
-def store(extracted):
-    row = extracted.split(',')
-    row = [item.strip() for item in row]
-    cursos = connection.cursor()
-    cursos.execute("INSERT INTO events VALUES(?,?,?)",row)
-    connection.commit()
-        
-def read(extracted):
-    row = extracted.split(',')
-    row = [item.strip() for item in row]
-    band,city,date = row
-    cursos = connection.cursor()
-    cursos.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band,city,date))
-    rows = cursos.fetchall()
-    print(rows)
-    return rows
+
+class DataBase:
+    def store(self,extracted):
+        row = extracted.split(',')
+        row = [item.strip() for item in row]
+        cursos = connection.cursor()
+        cursos.execute("INSERT INTO events VALUES(?,?,?)",row)
+        connection.commit()
+            
+    def read(self,extracted):
+        row = extracted.split(',')
+        row = [item.strip() for item in row]
+        band,city,date = row
+        cursos = connection.cursor()
+        cursos.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band,city,date))
+        rows = cursos.fetchall()
+        print(rows)
+        return rows
 
 if __name__ == '__main__':
     while True:
-        scraped = scrape(URL)
-        extracted = extract(scraped)
+        event = Event()   
+        dataBase = DataBase()
+        scraped = event.scrape(URL)
+        extracted = event.extract(scraped)
         print('extracted: ', extracted)
         if extracted != 'No upcoming tours':
-            row = read(extracted)          
+            row = dataBase.read(extracted)          
             if not row:
-                store(extracted)
+                dataBase.store(extracted)
                 send_email()
         time.sleep(2)
