76e52bc7b89f4fe0ee0bd8df2b0ec0f01d78cdc2
leowitcroz <leowitcroz@gmail.com>
2023-10-28 17:56:12 -0300

    __init__ in OOP

--- webscrapping/main.py
+++ webscrapping/main.py
@@ -8,7 +8,6 @@ URL = 'https://programmer100.pythonanywhere.com/tours/'
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-connection = sqlite3.connect('webscrapping/data.db')
 
 
 
@@ -30,18 +29,23 @@ def send_email():
     
 
 class DataBase:
+    
+    def __init__(self, database_path):
+        self.connection = sqlite3.connect(database_path)
+        
+    
     def store(self,extracted):
         row = extracted.split(',')
         row = [item.strip() for item in row]
-        cursos = connection.cursor()
+        cursos = self.connection.cursor()
         cursos.execute("INSERT INTO events VALUES(?,?,?)",row)
-        connection.commit()
+        self.connection.commit()
             
     def read(self,extracted):
         row = extracted.split(',')
         row = [item.strip() for item in row]
         band,city,date = row
-        cursos = connection.cursor()
+        cursos = self.connection.cursor()
         cursos.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band,city,date))
         rows = cursos.fetchall()
         print(rows)
@@ -50,11 +54,11 @@ class DataBase:
 if __name__ == '__main__':
     while True:
         event = Event()   
-        dataBase = DataBase()
         scraped = event.scrape(URL)
         extracted = event.extract(scraped)
         print('extracted: ', extracted)
         if extracted != 'No upcoming tours':
+            dataBase = DataBase(database_path='webscrapping/data.db')
             row = dataBase.read(extracted)          
             if not row:
                 dataBase.store(extracted)
