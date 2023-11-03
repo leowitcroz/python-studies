54889577e3b53cb785bdb1eec1391f29d95b4039
leowitcroz <leowitcroz@gmail.com>
2023-11-03 17:28:29 -0300

    book and avaliable methods

--- hotel_booking/main.py
+++ hotel_booking/main.py
@@ -1,20 +1,26 @@
 import pandas as pd   
 
-df = pd.read_csv('hotel_booking\hotels.csv')
+df = pd.read_csv('hotel_booking\hotels.csv', dtype={'id': str})
 class Hotel:
-    def __init__(self,id):
-        pass
+    def __init__(self,hotel_id):
+        self.hotel_id = hotel_id
     
     def book(self):
-        pass
+        df.loc[df["id"] == self.hotel_id, 'available'] = 'no'
+        df.to_csv('hotel_booking\hotels.csv', index=False)
     
     def avaliable(self):
-        pass
+        availability = df.loc[df["id"] == self.hotel_id, 'available'].squeeze()
+        if availability == 'yes':
+            return True
+        else: 
+            return False
+            
     
 
 
 class ReservationTicket:
-    def __init__(self,customers_name,hotel):
+    def __init__(self,customers_name,hotel_id):
         pass
     
     def generate(self):
@@ -23,9 +29,9 @@ class ReservationTicket:
 
 print(df)
 
-id = input('Enter de id of the hotel')
+hotel_id = input('Enter de id of the hotel: ')
 
-hotel = Hotel(id)
+hotel = Hotel(hotel_id)
 if hotel.avaliable():
     hotel.book()
     name = input('Enter your name: ')
