58b3b13585ad7e29dd49359fb4252af3dde45735
leowitcroz <leowitcroz@gmail.com>
2023-11-07 14:57:12 -0300

    credit card feature

--- hotel_booking/main.py
+++ hotel_booking/main.py
@@ -1,6 +1,7 @@
 import pandas as pd   
 
 df = pd.read_csv('hotel_booking\hotels.csv', dtype={'id': str})
+df_cards = pd.read_csv('hotel_booking\cards.csv', dtype=str).to_dict(orient='records')
 class Hotel:
     def __init__(self,hotel_id):
         self.hotel_id = hotel_id
@@ -33,6 +34,17 @@ class ReservationTicket:
         """
         return content
     
+class Creditcard:
+    def __init__(self, number):
+        self.number = number
+        
+    def validate(self,expiration,holder,cvc):
+        card_data = {'number':self.number, 'expiration':expiration, 'holder': holder, 'cvc':cvc}
+        if card_data in df_cards:
+            return True
+        else:
+            return False
+    
 
 print(df)
 
@@ -40,9 +52,11 @@ hotel_id = input('Enter de id of the hotel: ')
 
 hotel = Hotel(hotel_id)
 if hotel.avaliable():
-    hotel.book()
-    name = input('Enter your name: ')
-    reservation_ticket = ReservationTicket(name,hotel) 
-    print(reservation_ticket.generate())
+    credit_card = Creditcard(number="1234567890123456")
+    if credit_card.validate(expiration="12/26", holder="JOHN SMITH", cvc="123"):
+        hotel.book()
+        name = input('Enter your name: ')
+        reservation_ticket = ReservationTicket(name,hotel) 
+        print(reservation_ticket.generate())
 else: 
     print('This hotel is not free')
