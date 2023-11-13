6b188485e9047e5fd49fc6c7cc6499994cc847fa
leowitcroz <leowitcroz@gmail.com>
2023-11-12 23:01:30 -0300

    populating table with db data

--- student_management/main.py
+++ student_management/main.py
@@ -1,8 +1,9 @@
 import PyQt6
 import sys
 from datetime import datetime
-from PyQt6.QtWidgets import  QApplication, QVBoxLayout, QLabel, QWidget,QGridLayout,QLineEdit,QPushButton,QMainWindow,QTableWidget
+from PyQt6.QtWidgets import  QApplication, QVBoxLayout, QLabel, QWidget,QGridLayout,QLineEdit,QPushButton,QMainWindow,QTableWidget,QTableWidgetItem
 from PyQt6.QtGui import QAction
+import sqlite3
 
 
 class MainWindow(QMainWindow):
@@ -25,9 +26,19 @@ class MainWindow(QMainWindow):
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(('Id', 'Name', 'Course', 'Mobile'))
         self.setCentralWidget(self.table)
+        self.load_data()
         
     def load_data(self):
-        self.table
+        connection = sqlite3.connect('student_management\database.db')
+        result = connection.execute("SELECT * FROM students")
+        self.table.setRowCount(0)
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+        connection.close()
+        
+        
         
         
         
